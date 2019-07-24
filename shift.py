93bbbe4356a3bad4e48850207e834b66a3f0e3aa
itwasntzak <51720888+itwasntzak@users.noreply.github.com>
2019-07-24 00:59:23 -0600

    Update shift.py

--- shift.py
+++ shift.py
@@ -1,27 +1,29 @@
 import main
 
-
 def startShift():
-    with open(main.date() + '.txt', 'w') as today:
+    with open(main.date() + '.py', 'w') as today:
         today.write('startShift = ' + "'" + main.time() + "'")
 
 
 def endShift():
-    with open(main.date() + '.txt', 'a+') as today:
+    with open(main.date() + '.py', 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nendShift = ' + "'" + main.time() + "'")
 
+    with open('dlvNumb.txt', 'w') as dlvNumb:
+        dlvNumb.write('0')
+
 
 def startSplit():
-    with open(main.date() + '.txt', 'a+') as today:
+    with open(main.date() + '.py', 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nstartSplit = ' + "'" + main.time() + "'")
 
 
 def endSplit():
-    with open(main.date() + '.txt', 'a+') as today:
+    with open(main.date() + '.py', 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nendSplit = ' + "'" + main.time() + "'")
