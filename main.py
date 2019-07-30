2ac23f5e8a2f0c32714e22be7812d5189c1ca4f7
itwasntzak_window <zakary100@gmail.com>
2019-07-30 12:29:46 -0600

    making changes to run on things other then termux

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def now():
 
 def startMenu():
     while True:
-        if os.path.exists(os.path.join("deliveryTracking", "shifts", str(now().date()) + '.py')) == False:
+        if os.path.exists(os.path.join("shifts", str(now().date()) + '.py')) == False:
             print('what would you like to do?\n1 to start a new shifts | 2 to return from split')
             try:
                 userInput = int(input())
@@ -31,7 +31,7 @@ def startMenu():
                 print('\ninvalid input...')
 
 
-        elif os.path.exists(os.path.join("deliveryTracking", "shifts", str(now().date()) + '.py')) == True:
+        elif os.path.exists(os.path.join("shifts", str(now().date()) + '.py')) == True:
             while True:
                 print('what would you like to do?\n1 to continue shift | 2 to return from split | 0 to overwrite shift')
                 try:
@@ -79,10 +79,10 @@ def shiftMenu():
                 break
 
         except ValueError:
-            print('invalid input...')
+            print('\ninvalid input...')
 
         else:
-            print('invalid input...')
+            print('\ninvalid input...')
 
 
 if __name__ == "__main__":
--- shift.py
+++ shift.py
@@ -2,29 +2,29 @@ import os
 import main
 
 def startShift():
-    with open(os.path.join("deliveryTracking", "shifts", str(main.now().date()) + '.py'), 'w') as today:
+    with open(os.path.join("shifts", str(main.now().date()) + '.py'), 'w') as today:
         today.write('startShift = ' + str(main.now()))
 
 
 def endShift():
-    with open(os.path.join("deliveryTracking", "shifts", str(main.now().date()) + '.py'), 'a+') as today:
+    with open(os.path.join("shifts", str(main.now().date()) + '.py'), 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nendShift = ' + str(main.now()))
 
-    with open(os.path.join("deliveryTracking", "dlvNumb.txt"), 'w') as dlvNumb:
+    with open(os.path.join("dlvNumb.txt"), 'w') as dlvNumb:
         dlvNumb.write('0')
 
 
 def startSplit():
-    with open(os.path.join("deliveryTracking", "shifts", str(main.now().date()) + '.py'), 'a+') as today:
+    with open(os.path.join("shifts", str(main.now().date()) + '.py'), 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nstartSplit = ' + str(main.now()))
 
 
 def endSplit():
-    with open(os.path.join("deliveryTracking", "shifts", str(main.now().date()) + '.py'), 'a+') as today:
+    with open(os.path.join("shifts", str(main.now().date()) + '.py'), 'a+') as today:
         content = today.read()
         today.seek(len(content))
         today.write('\nendSplit = ' + str(main.now()))
