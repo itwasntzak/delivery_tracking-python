ed3f215b6f608c84042ed571532a1fcddf702d33
itwasntzak_window <zakary100@gmail.com>
2019-08-05 19:59:34 -0600

    making many major changes, to completely functional

--- delivery.py
+++ delivery.py
@@ -1,64 +1,146 @@
 import os
-import order
-import main
+
 import utilFunc
+import order
 
 
-def endOrder(startTime):
+def onDelivery(option):
     while True:
-        print('\ndriving to address...\n1 after returning to vehicle')
+        print('\n' + option + '\n1 after returning | 2 for extra stop')
         try:
             waitForUser = int(input())
-
             if waitForUser == 1:
-                ord = order.order()
-                utilFunc.timeTook(startTime, ord[1][2], 'order')
-                return ord
+                break
+
+            elif waitForUser == 2:
+                extraStop()
+                continue
 
         except ValueError:
             print('\ninvalid input')
-
         else:
             print('\ninvalid input')
 
 
-def numbOfOrders(startTime):
+def extraStop():
+    while True:
+        print('\nmaking extra stop...\n1 to continue')
+        try:
+            waitForUser = int(input())
+            if waitForUser == 1:
+
+                while True:
+                    print('\nname of extra stop:')
+
+                    try:
+                        extraStopName = input()
+
+                        if extraStopName.isdigit() == False:
+                            areYouSure = utilFunc.areYouSure(extraStopName + '.')
+
+                            if areYouSure == True:
+                                while True:
+                                    print('\nwhat was the reason for the extra stop?')
+
+                                    try:
+                                        extraStopReason = str(input())
+
+                                        if extraStopReason.isdigit() == False:
+                                            areYouSure2 = utilFunc.areYouSure(extraStopReason + '.')
+
+                                            if areYouSure2 == True:
+                                                utilFunc.writeData("delivery", extraStopName + "EndTime.txt", utilFunc.now())
+                                                utilFunc.writeData("delivery", extraStopName + "MilesTrav.txt", milesTrav('extra'))
+                                                utilFunc.writeData("delivery", extraStopName + "Reason.txt", "'" + extraStopReason + "'")
+                                                return
+
+                                            else:
+                                                continue
+
+                                        else:
+                                            print('\ninvalid input...')
+                                            continue
+
+                                    except ValueError:
+                                        print('\ninvalid input')
+
+                        else:
+                            print('\ninvalid input')
+                            continue
+
+                    except ValueError:
+                        print('\ninvalid input')
+
+
+        except ValueError:
+            print('\ninvalid input')
+        else:
+            print('\ninvalid input')
+
+
+def milesTrav(varPath, varWord=''):
+    while True:
+        print('\n' + varWord + 'miles traveled:')
+        try:
+            milesTravInput = float(input())
+            utilFunc.writeData("delivery", str(varPath) + 'MilesTrav.txt', str(milesTravInput))
+            break
+
+        except ValueError:
+            print('\ninvalid input...')
+
+
+def numbOfOrders():
     while True:
         print('\nnumber of orders:')
         try:
             numbOfOrders = int(input())
 
-            if numbOfOrders == 1:
-                return [numbOfOrders, endOrder(startTime)]
+            if numbOfOrders >= 1:
+                utilFunc.writeData("delivery", "numbOfOrders.txt", numbOfOrders)
 
-            elif numbOfOrders > 1:
-                return [numbOfOrders, [endOrder(startTime) for value in range(numbOfOrders)]]
-
-        except ValueError:
-            print('\ninvalid input')
-
-
-def delivery(startTime):
-    orders = numbOfOrders(startTime)
-    
-    while True:
-        print('\nreturning to store...\nupon arival, 1 to continue')
-        try:
-            waitForUser = int(input())
-            if waitForUser == 1:
-                return orders
+                return numbOfOrders
 
         except ValueError:
             print('\ninvalid input')
 
 
-def createDelivery():
-    startTime = utilFunc.now()
-    dlv = delivery(startTime)
-    totalMilesTrav = utilFunc.milesTrav('total ')
-    endTime = utilFunc.now()
-    
-    utilFunc.timeTook(startTime, endTime, 'delivery')
+def delivery():
+    os.mkdir(os.path.join("delivery"))
+    utilFunc.writeData("delivery", "deliveryStartTime.txt", str(utilFunc.now()))
+    numberOfOrder = int(numbOfOrders())
 
-    with open(os.path.join("shifts", str(utilFunc.now().date()) + '.py'), 'a') as today:
-        today.write('\ndlv' + str(utilFunc.deliveryNumb('number')) + ' = ' + str([startTime, dlv, totalMilesTrav, endTime]))
+    if numberOfOrder == 1:
+        onDelivery('driving to address...')
+
+        orderNumb = order.orderNumb()
+
+        utilFunc.writeData("delivery", str(orderNumb) + "EndTime.txt", str(utilFunc.now()))
+
+        order.tip(orderNumb)
+
+        milesTrav(orderNumb)
+
+        onDelivery('driving back to store...')
+
+        milesTrav('total', 'total ')
+
+
+    elif numberOfOrder >= 1:
+        for value in range(numberOfOrder):
+            onDelivery('driving to address...')
+
+            orderNumb = order.orderNumb()
+
+            order.tip(orderNumb)
+
+            milesTrav(orderNumb)
+
+            utilFunc.writeData("delivery", str(orderNumb) + "EndTime.txt", str(utilFunc.now()))
+
+        onDelivery('driving back to store...')
+
+        milesTrav('total', 'total ')
+
+    utilFunc.writeData("delivery", "deliveryEndTime.txt", str(utilFunc.now()))
+    utilFunc.writeData("shift", "numbOfDeliveries.txt", str(int(utilFunc.deliveryNumb('number')) + 1))
--- shift.py
+++ shift.py
@@ -4,27 +4,19 @@ import main
 import utilFunc
 
 def startShift():
-    with open(os.path.join("shifts", str(utilFunc.now().date()) + '.py'), 'w') as today:
-        today.write('startShift = ' + str(utilFunc.now()))
+    utilFunc.deliveryNumb('reset')
+    os.mkdir(os.path.join("shift"))
+    utilFunc.writeData("shift", "shiftStartTime.txt", utilFunc.now())
 
 
 def endShift():
-    with open(os.path.join("shifts", str(utilFunc.now().date()) + '.py'), 'a+') as today:
-        content = today.read()
-        today.seek(len(content))
-        today.write('\nendShift = ' + str(utilFunc.now()))
-        utilFunc.deliveryNumb('reset')
+    utilFunc.writeData("shift", "shiftEndTime.txt", utilFunc.now())
+    utilFunc.deliveryNumb('reset')
 
 
 def startSplit():
-    with open(os.path.join("shifts", str(utilFunc.now().date()) + '.py'), 'a+') as today:
-        content = today.read()
-        today.seek(len(content))
-        today.write('\nstartSplit = ' + str(utilFunc.now()))
+    utilFunc.writeData("shift", "splitStartTime.txt", utilFunc.now())
 
 
 def endSplit():
-    with open(os.path.join("shifts", str(utilFunc.now().date()) + '.py'), 'a+') as today:
-        content = today.read()
-        today.seek(len(content))
-        today.write('\nendSplit = ' + str(utilFunc.now()))
+    utilFunc.writeData("shift", "splitEndTime.txt", utilFunc.now())
