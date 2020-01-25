014ee4344b332bbf070339bdbf4de66db78b9f2d
itwasntzak_window <zakary100@gmail.com>
2020-01-24 22:10:36 -0700

    fixing directory change

--- delivery_tracking.py
+++ delivery_tracking.py
@@ -4,75 +4,6 @@
 
 from os import path, chdir
 
-<<<<<<< HEAD
-from continuation import continuation
-import extra_stop
-import menu_options
-import input_data
-import shift
-import utility
-
-
-shift_start_time_path = path.join('shift', 'shift_start_time.txt')
-shift_end_time_path = path.join('shift', 'shift_end_time.txt')
-split_start_time_path = path.join('shift', 'split_start_time.txt')
-split_end_time_path = path.join('shift', 'split_end_time.txt')
-
-
-# //TODO: need to add logic for split_info.txt
-def start():
-    chdir('delivery_tracking')
-    continuation()
-    while True:
-        # check if shift has started
-        if not path.exists(shift_start_time_path):
-            menu_options.new_shift()
-        # check if shift has ended
-        elif path.exists(shift_end_time_path):
-            menu_options.ended_shift()
-        # check if a split has or has not been started or ended
-        elif path.exists(shift_start_time_path)\
-                and not path.exists(split_start_time_path)\
-                or path.exists(split_end_time_path):
-            shift_menu()
-            menu_options.continue_shift()
-        # check if split has been started
-        elif path.exists(shift_start_time_path)\
-                and path.exists(split_start_time_path)\
-                and not path.exists(split_end_time_path):
-            menu_options.end_split()
-
-
-def shift_menu():
-    shift_object = shift.Shift()
-    shift_object.start_time = datetime.strptime(
-        utility.read_data(
-            path.join('shift', 'shift_start_time.txt')),
-        '%Y-%m-%d %H:%M:%S.%f')
-    while True:
-        user_choice = input_data.get_input(
-            prompt='\nWhat would you like to do?'
-                   '\n1 to start delivery '
-                   '| 2 to end shift '
-                   '| 3 to start split '
-                   '| 4 to start an extra stop '
-                   '| 0 for start menu\n',
-            kind=int)
-        if user_choice == 1:
-            menu_options.start_delivery()
-        elif user_choice == 2:
-            shift.end_shift()
-        elif user_choice == 3:
-            shift.start_split()
-        elif user_choice == 4:
-            extra_stop_start_time_path = path.join(
-                'shift', 'extra_stop_start_time.txt')
-            utility.write_data(extra_stop_start_time_path, utility.now())
-            # //TODO: write a menu option function for extra_stop, enter to cont
-            extra_stop.extra_stop(shift_object)
-        elif user_choice == 0:
-            break
-=======
 from delivery import Delivery
 from extra_stop import Extra_Stop
 from order import Order
@@ -83,7 +14,7 @@ from utility import driving
 
 
 
-# chdir('delivery_tracking')
+chdir('delivery_tracking')
 while True:
     shift_object = Shift()
     # check if shift has started
@@ -112,7 +43,6 @@ while True:
         # check if driving to address is in progress
         elif path.exists(path.join('delivery', 'driving-address')):
             driving(delivery_object, '\nDriving to address...', 'address')
->>>>>>> test
         else:
             delivery_object.resume()
     # check if split has been started
