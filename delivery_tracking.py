2ae7efcab8206f57dfb07b38350667d84fb98ff8
itwasntzak_window <zakary100@gmail.com>
2020-03-11 02:57:05 -0600

    rming chdir and getting current days shift files

--- delivery_tracking.py
+++ delivery_tracking.py
@@ -2,13 +2,12 @@
 # cnsd: add option to be able to start a second shift for the day (dif store)
 
 from input_data import get_input
-from os import chdir, path, mkdir
+from os import path, mkdir
 from shift import shift_menu, Shift
 from split import Split
 from utility import now
 
 
-chdir('delivery_tracking')
 if not path.exists('shifts'):
     mkdir('shifts')
 while True:
