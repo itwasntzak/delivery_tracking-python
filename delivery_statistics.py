e0c3a3c24a8e14b119ec2d361a0d771a5a3ab763
itwasntzak_window <zakary100@gmail.com>
2020-03-13 23:10:49 -0600

    fixing to work on termux

--- delivery_statistics.py
+++ delivery_statistics.py
@@ -1,3 +1,5 @@
+from os import chdir
+
 import input_data
 import shift
 
@@ -83,4 +85,5 @@ def select_statistic(shifts_list):
             print('\nInvalid input...\n\n')
 
 
+chdir('delivery_tracking')
 select_statistic(select_shifts())
